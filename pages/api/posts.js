e2edba0ebf422e5cbdff352c9b8f3c170fd09611
Phillip H <phillip.hickinbotham@sunderlandsoftwarecity.com>
2024-11-18 17:02:03 +0000

    first commit

---
+++ pages/api/posts.js
@@ -0,0 +1,19 @@
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+export default async function handler(req, res) {
+  if (req.method === 'GET') {
+    const posts = await prisma.post.findMany();
+    res.json(posts);
+  } else if (req.method === 'POST') {
+    const { title, content } = req.body;
+    const post = await prisma.post.create({
+      data: {
+        title,
+        content,
+      },
+    });
+    res.json(post);
+  }
+}
